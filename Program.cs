b8526f74c1797e4a28fcbb5a709011491a290112
Woselko <wojciech.wlas@wp.pl>
2023-12-28 12:58:13 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Numerology App in RaspBerry !!!");
+Console.WriteLine("Type your Name: ");
+string userInput = Console.ReadLine();
+int numerologyValue = CalculateNumerologyValue(userInput);
+Console.WriteLine("Your magic numbers for your name '{0}' is: {1}", userInput, numerologyValue);
+      
+ static int CalculateNumerologyValue(string input)
+ {
+     int value = 0;
+     input = input.ToLower();
+     foreach (char character in input)
+     {
+         if (char.IsLetter(character))
+         {
+             int charValue = character - 'a' + 1;
+             value += charValue;
+         }
+     }
+     while (value > 9)
+     {
+         int sum = 0;
+         foreach (char digit in value.ToString())
+         {
+             sum += int.Parse(digit.ToString());
+         }
+         value = sum;
+     }
+     return value;
+ }
